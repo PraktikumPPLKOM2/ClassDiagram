76bcf14bac20b0cc25d15d69b3f9a734e325c52b
Wilbert Taurean <33562619+razerbest01@users.noreply.github.com>
2020-11-09 19:03:34 +0700

    .

--- main.py
+++ main.py
@@ -3,10 +3,32 @@ import kelas
 
 # MAIN
 
-user = {}
-
+# Login Session
 newuser = loginsession()
+while(True):
+    print("SELAMAT DATANG ")
+    print('''
+    1. Login
+    2. Register
+    3. Forgot Password
+    4. keluar
+    ''')
+    a = input("masukkan pilihan anda: ")
+    if (a == "1"):
+        newuser.login()
+    elif (a == "2"):
+        newuser.register()
+    elif (a == "3"):
+        newpass = newuser.forgotpassword()
 
+    elif(a==4):
+        print("Sampai jumpa lagi")
+        break
+    else:
+        print("salah memasukkan pilihan")
+
+
+'''
 # GURU
 newuser.register()
 user["Guru"] = newuser
@@ -21,4 +43,4 @@ newuser.login()
 newuser.register()
 user["Ortu"] = newuser
 newuser.login()
-
+'''
